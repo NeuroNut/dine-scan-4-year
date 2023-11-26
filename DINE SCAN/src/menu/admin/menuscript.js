89f3ff498061a47f88ee992ed8dfea2a9c360752
Aditya Agarwal <118915506+NeuroNut@users.noreply.github.com>
2023-11-27 00:09:44 +0530

    Update menuscript.js

--- DINE SCAN/src/menu/admin/menuscript.js
+++ DINE SCAN/src/menu/admin/menuscript.js
@@ -1,43 +1,62 @@
 const foodForm = document.getElementById("foodForm");
 
 foodForm.addEventListener("submit", async function (event) {
-  event.preventDefault();
+    //event.preventDefault();
+  
+    const foodName = document.getElementById("foodName").value;
+    const foodImage = document.getElementById("foodImage").files[0]; // Get the selected image file
+    const foodDescription = document.getElementById("foodDescription").value;
+    const foodCategory = document.getElementById("foodCategory").value;
+    const foodVisibility = document.getElementById("foodVisibility").value; 
+    const optionElements = optionsContainer.getElementsByClassName("option-group");
+     
+  
+      // Store the size options and costs
+      const sizeOptions = Array.from(optionElements).map((option, i) => option.querySelector(`select[name="option[${i}]"]`).value);
+      const costs = Array.from(optionElements).map((option, i) => option.querySelector(`input[name="cost[${i}]"]`).value);
+  
+  
+    const newFoodItem = {
+      name: foodName,
+      description: foodDescription,
+      category: foodCategory,
+      visibility: foodVisibility,
+      sizeOptions: sizeOptions,
+      costs: costs
+    };
+  
+    console.log("New Food Item:", newFoodItem);
+  
+    const response = await saveToDatabase(newFoodItem, foodImage);
 
-  const foodName = document.getElementById("foodName").value;
-  const foodImage = document.getElementById("foodImage").files[0]; // Get the selected image file
-  const foodDescription = document.getElementById("foodDescription").value;
-  const foodCost = document.getElementById("foodCost").value;
-
-  const newFoodItem = new FormData(); // Create a new FormData object
-  newFoodItem.append("name", foodName);
-  newFoodItem.append("image", foodImage); // Append the image file to the FormData
-  newFoodItem.append("description", foodDescription);
-  newFoodItem.append("cost", foodCost);
-
-  console.log("New Food Item:", newFoodItem);
-
-  const response = await saveToDatabase(newFoodItem);
-
-  if (response.success) {
-    alert("Food item added successfully!");
-    foodForm.reset();
-  } else {
-   // console.log(response.message);
-    alert("Failed to add food item. Please try again.");
+    if (response.success) {
+      
+      console.log("the insert id is: ", response.id);
+      alert("Food item added successfully.");
+    } else {
+        // console.log(response);
+        // console.log("the insert id is: ", response.id);
+        // console.log(response.message);
+        alert("Failed to add food item without image. Please try again.");
+        }
+   });
+  
+   async function saveToDatabase(foodItem, imageFile) {
+    try {
+      const formData = new FormData();
+      formData.append('image', imageFile);
+      formData.append('foodItem', JSON.stringify(foodItem));
+  
+      const response = await fetch('save_food_item.php', {
+        method: 'POST',
+        body: formData
+      });
+  
+      const responseData = await response.json();
+      console.log(responseData);
+      return responseData;
+    } catch (error) {
+      console.error('Error saving data to the database:', error);
+      return { success: false, message: error };
+    }
   }
-});
-
-async function saveToDatabase(foodItem) {
-  try {
-    console.log("Food Item:", foodItem);
-    const response = await fetch("save_food_item.php", {
-      method: "POST",
-      body: foodItem, // Send the FormData object as the request body
-    });
-
-    return await response.json();
-  } catch (error) {
-    console.error("Error saving data to the database:", error);
-    return { success: false };
-  }
-}

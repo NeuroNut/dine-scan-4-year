4ff2d7ac049d57e3171f604f8c064066ecc43c51
Aditya Agarwal <118915506+NeuroNut@users.noreply.github.com>
2023-11-27 00:08:48 +0530

    Update save_food_item.php

--- DINE SCAN/src/menu/admin/save_food_item.php
+++ DINE SCAN/src/menu/admin/save_food_item.php
@@ -1,5 +1,7 @@
 <?php
 // Establish a database connection
+
+
 require_once("../../DB/db_conn.php");
 if ($conn === false) {
     die("Connection failed: " . mysqli_connect_error());
@@ -7,11 +9,18 @@ if ($conn === false) {
 
 // Check if data is sent via POST
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Your code goes here
-
-    $name = mysqli_real_escape_string($conn, $_POST['name']);
-    $description = mysqli_real_escape_string($conn, $_POST['description']);
-    $cost = floatval($_POST['cost']);
+    
+    if (isset($_POST['foodItem'])) {
+        $foodItem = json_decode($_POST['foodItem'], true); // true makes it an associative array
+    
+        // Now you can access the values like this:
+        $name = $foodItem['name'];
+        $description = $foodItem['description'];
+        $category = $foodItem['category'];
+        $visibility = $foodItem['visibility'];
+        $sizes = $foodItem['sizeOptions'];
+        $costs = $foodItem['costs'];
+    }
 
     if (isset($_FILES["image"]) && isset($_FILES["image"]["tmp_name"]) && !empty($_FILES["image"]["tmp_name"])) {
         $imageData = file_get_contents($_FILES["image"]["tmp_name"]);
@@ -24,16 +33,29 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     error_log($imageMessage); // Log image message to the console
 
-    // Inserting data into the database
-    $query = "INSERT INTO menu_items (name, image, description, cost) VALUES ('$name', '$base64Image', '$description', $cost)";
+    // $sizes = isset($formData['sizeOptions']) ? $formData['sizeOptions'] : [];
+    // $costs = isset($formData['costs']) ? $formData['costs'] : [];
 
-    if (mysqli_query($conn, $query)) {
-        $response = array("success" => true, "message" => "Food item added successfully!");
-    } else {
-        error_log("Error: " . $query . "<br>" . $conn->error); // Log database error to the console
-        $response = array("success" => false, "message" => "Failed to add food item. Please try again.");
+    // Inserting data into the database
+    $query = "INSERT INTO menu_items (name, image, description, category_id, visibility) VALUES (?,?,?,?,?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('sssis', $name, $base64Image, $description, $category, $visibility);
+    $stmt->execute();
+    $menuItemID = $stmt->insert_id;
+
+    //for loop iterating through the sizes and costs arrays
+    for($i = 0; $i < count($sizes); $i++) {
+        $size = $sizes[$i];
+        $cost = $costs[$i];
+        $query = "INSERT INTO menu_sizesNcosts (menu_id, size_id, cost) VALUES (?,?,?)";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('iid', $menuItemID, $size, $cost);
+        $stmt->execute();
     }
 
+    //send back a success response
+    $response = ["success" => true, "message" => "Food item added successfully!", "id" => $menuItemID];
+   
     echo json_encode($response);
 } else {
     // If Not a POST request
@@ -50,4 +72,5 @@ function getDefaultImage() {
         die("Error: Default image not found at '$defaultImagePath'.");
     }
 }
+
 ?>
